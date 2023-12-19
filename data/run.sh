fa62bcf732f4ec1803bb45f7ef27e45f2db72d6e
Hieu Le <sudo@rm-rf.cloud>
2023-12-19 10:35:02 +0700

    add runtime params for agent support QUIC

--- data/run.sh
+++ data/run.sh
@@ -2,6 +2,7 @@
 set -e
 
 RELAY_SERVER=$(bashio::config 'tcp_connector_addr')
+PROTOCOL=$(bashio::config 'connector_protocol')
 HTTP_DEST=$(bashio::config 'http_dest')
 HTTPS_DEST=$(bashio::config 'https_dest')
 KEYFILE="/ssl/$(bashio::config 'local_key')"
@@ -9,7 +10,8 @@ KEYFILE="/ssl/$(bashio::config 'local_key')"
 bashio::log.info "Starting 8xFF Agent..."
 
 exec /agent \
-    --tcp-connector-addr "$RELAY_SERVER" \
+    --connector-addr "$RELAY_SERVER" \
+    --connector-protocol "$PROTOCOL" \
     --http-dest "$HTTP_DEST" \
     --https-dest "$HTTPS_DEST" \
     --local-key "$KEYFILE" \
